d478c6826a63682fb30cc9624d387549a28be8e0
erdem koyuncu <erdemfb1907@outlook.com>
2025-07-06 13:48:03 +0300

    Updated tailwind, added theme selection

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,6 +13,8 @@ import { NotFound } from "~/components/NotFound";
 import appCss from "~/styles/app.css?url";
 import { seo } from "~/utils/seo";
 import { QueryClient } from "@tanstack/react-query";
+import { ThemeProvider } from "~/components/theme-provider";
+import { ModeToggle } from "~/components/ui/mode-toggle";
 
 export const Route = createRootRouteWithContext<{
   QueryClient: QueryClient;
@@ -68,68 +70,71 @@ export const Route = createRootRouteWithContext<{
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        <div className="p-2 flex gap-2 text-lg">
-          <Link
-            to="/"
-            activeProps={{
-              className: "font-bold",
-            }}
-            activeOptions={{ exact: true }}
-          >
-            Home
-          </Link>{" "}
-          <Link
-            to="/posts"
-            activeProps={{
-              className: "font-bold",
-            }}
-          >
-            Posts
-          </Link>{" "}
-          <Link
-            to="/users"
-            activeProps={{
-              className: "font-bold",
-            }}
-          >
-            Users
-          </Link>{" "}
-          <Link
-            to="/route-a"
-            activeProps={{
-              className: "font-bold",
-            }}
-          >
-            Pathless Layout
-          </Link>{" "}
-          <Link
-            to="/deferred"
-            activeProps={{
-              className: "font-bold",
-            }}
-          >
-            Deferred
-          </Link>{" "}
-          <Link
-            // @ts-expect-error
-            to="/this-route-does-not-exist"
-            activeProps={{
-              className: "font-bold",
-            }}
-          >
-            This Route Does Not Exist
-          </Link>
-        </div>
-        <hr />
-        {children}
-        <TanStackRouterDevtools position="bottom-right" />
-        <Scripts />
-      </body>
-    </html>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <html>
+        <head>
+          <HeadContent />
+        </head>
+        <body className="min-h-[100dvh]">
+          <div className="p-2 flex gap-2 text-lg">
+            <Link
+              to="/"
+              activeProps={{
+                className: "font-bold",
+              }}
+              activeOptions={{ exact: true }}
+            >
+              Home
+            </Link>{" "}
+            <Link
+              to="/posts"
+              activeProps={{
+                className: "font-bold",
+              }}
+            >
+              Posts
+            </Link>{" "}
+            <Link
+              to="/users"
+              activeProps={{
+                className: "font-bold",
+              }}
+            >
+              Users
+            </Link>{" "}
+            <Link
+              to="/route-a"
+              activeProps={{
+                className: "font-bold",
+              }}
+            >
+              Pathless Layout
+            </Link>{" "}
+            <Link
+              to="/deferred"
+              activeProps={{
+                className: "font-bold",
+              }}
+            >
+              Deferred
+            </Link>{" "}
+            <Link
+              // @ts-expect-error
+              to="/this-route-does-not-exist"
+              activeProps={{
+                className: "font-bold",
+              }}
+            >
+              This Route Does Not Exist
+            </Link>
+            <ModeToggle />
+          </div>
+          <hr />
+          {children}
+          <TanStackRouterDevtools position="bottom-right" />
+          <Scripts />
+        </body>
+      </html>
+    </ThemeProvider>
   );
 }
