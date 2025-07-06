7d9155aa36c16fe25a687e7149b67280df6e2a1a
erdem koyuncu <erdemfb1907@outlook.com>
2025-07-06 13:21:03 +0300

    First connection is done between convex and tanstack start

--- src/routes/another.tsx
+++ src/routes/another.tsx
@@ -1,9 +1,23 @@
 import { createFileRoute } from "@tanstack/react-router";
+// import { SignIn } from "~/components/SignIn";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { convexQuery } from "@convex-dev/react-query";
+import { api } from "convex/_generated/api";
 
 export const Route = createFileRoute("/another")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/another"!</div>;
+  const { data } = useSuspenseQuery(convexQuery(api.tasks.get, {}));
+
+  return (
+    <div>
+      <p>Hello "/another"!</p>
+      {/* <SignIn /> */}
+      {data.map(({ _id, text }) => (
+        <div key={_id}>{text}</div>
+      ))}
+    </div>
+  );
 }
