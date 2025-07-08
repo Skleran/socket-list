467d94c30b1dae1152c7e15136511900d572131e
erdem koyuncu <erdemfb1907@outlook.com>
2025-07-08 13:06:12 +0300

    Migrated project to next

--- src/components/SignIn.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuthActions } from "@convex-dev/auth/react";
-
-export function SignIn() {
-  const { signIn } = useAuthActions();
-  return (
-    <button onClick={() => void signIn("github")}>Sign in with GitHub</button>
-  );
-}
